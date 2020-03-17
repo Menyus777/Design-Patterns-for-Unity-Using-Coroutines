bfff01d2adb975ea3dc35becd361dafa261affe8
Menyus-Asus_G <menyus777@gmai.com>
2020-03-17 19:48:23 +0100

    Init upload of Threaded Coroutine

---
+++ Assets/Scripts/Coroutine Design Patterns/Threaded Coroutine/Accessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Accessor : MonoBehaviour
+{
+    [SerializeField]
+    private Dictionary<Guid, bool> CoroutineThreads = new Dictionary<Guid, bool>();
+
+    public void StartCoroutineThread(CoroutineThread coroutineThread, bool isLongRunning = false) // Long running: Since tasks still run on the ThreadPool, they should not be used for long-running operations, since they can still fill up the thread pool and block new work. Instead, Task provides a LongRunning option, which will tell the TaskScheduler to spin up a new thread rather than running on the ThreadPool.
+    {
+        CoroutineThreads.Add(coroutineThread.ID, false);
+        StartCoroutine(RunCoroutineThread(coroutineThread, isLongRunning));
+    }
+
+    private IEnumerator RunCoroutineThread(CoroutineThread coroutineThread, bool isLongRunning)
+    {
+        Task coroutineTask;
+        if (isLongRunning)
+            coroutineTask = Task.Factory.StartNew(() => coroutineThread.Work(), TaskCreationOptions.LongRunning);
+        else
+            coroutineTask = Task.Run(() => coroutineThread.Work());
+        while (!coroutineThread.IsFinished)
+        {
+            Debug.Log("<color=yellow>Accessor method Thread ID:</color> " + Thread.CurrentThread.ManagedThreadId);
+            yield return coroutineThread;
+
+            Debug.Log("Need data from the Main Thread in frame " + Time.frameCount);
+            CoroutineThreads[coroutineThread.ID] = true;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Coroutine Design Patterns/Threaded Coroutine/CoroutineThread.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CoroutineThread : IEnumerator
+{
+    /// <summary>
+    /// The unique identifier of the coroutine thread
+    /// </summary>
+    public Guid ID { get; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Indicated whether the <see cref="CoroutineThread"/> finished executing or not.
+    /// </summary>
+    public bool IsFinished { get; } = false;
+
+
+    volatile bool RequestMainThread = false;
+
+    public void Work()
+    {
+        Debug.Log("<color=yellow>Run method Thread ID:</color> " + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(5000);
+        RequestMainThread = true;
+    }
+
+    #region Yielder
+    public object Current { get { return null; } }
+
+    public bool MoveNext()
+    {
+        if (!RequestMainThread)
+        {
+            return true;
+        }
+        else
+        {
+            RequestMainThread = false;
+            return false;
+        }
+    }
+
+    public void Reset() { }
+
+    #endregion
+}
