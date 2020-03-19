3a22e1751baa546273e345159219c7e81ad7fc2f
Menyus-Asus_G <menyus777@gmai.com>
2020-03-19 23:14:42 +0100

    Added an explicit interface to prevent programming errors

---
+++ Assets/Scripts/Coroutine Design Patterns/Threaded Coroutine/IStartThreadedCoroutine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+/// <summary>
+/// An explicitly implemented interface for <see cref="ThreadedCoroutine"/> to prevent programmers accidentally
+/// calling <see cref="StartWithCoroutineThread(bool, System.Threading.CancellationToken)"/> and <see cref="StartWithUnityThread(bool, System.Threading.CancellationToken)"/>
+/// rather than via <see cref="ThreadedCoroutineManager"/>
+/// </summary>
+public interface IStartThreadedCoroutine
+{
+    /// <summary>
+    ///  This method shall be called by <see cref="ThreadedCoroutineManager"/> when starting a <see cref="ThreadedCoroutine"/> with the Unity thread first, 
+    ///  so the <see cref="ThreadedCoroutine"/> starts with traditional a coroutine part first
+    /// </summary>
+    IEnumerator StartWithUnityThread(bool isLongRunning, System.Threading.CancellationToken cancellationToken);
+
+    /// <summary>
+    ///  This method shall be called by <see cref="ThreadedCoroutineManager"/> when starting a <see cref="ThreadedCoroutine"/> with the underlying task thread first, 
+    ///  so the <see cref="ThreadedCoroutine"/> starts with a task runniong either on a Threadpool or on a standalone thread
+    /// </summary>
+    IEnumerator StartWithCoroutineThread(bool isLongRunning, System.Threading.CancellationToken cancellationToken);
+}
