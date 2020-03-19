2773f35cf6c0270112d12df2aae867379d2de928
Menyus-Asus_G <menyus777@gmai.com>
2020-03-19 23:15:53 +0100

    Disposing of unmanaged CancellationTokenSource

--- Assets/Scripts/Coroutine Design Patterns/Threaded Coroutine/ThreadedCoroutineManager.cs
+++ Assets/Scripts/Coroutine Design Patterns/Threaded Coroutine/ThreadedCoroutineManager.cs
@@ -10,17 +10,29 @@ using UnityEngine;
 /// </summary>
 public class ThreadedCoroutineManager : MonoBehaviour
 {
+    static readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
     /// <summary>
     /// Starts a Coroutine on the threadpool or on a new thread
     /// </summary>
     /// <param name="coroutineThread">The <see cref="ThreadedCoroutine"/> to start</param>
-    /// <param name="isLongRunning">Set this to true if your task is a long running one in order to avoid filling up the threadpool </param>
+    /// <param name="isLongRunning">Set this to true if your coroutine will be a long running one in order to avoid filling up the threadpool</param>
+    /// <param name="threadStarts">Set this to true if the <see cref="ThreadedCoroutine"/> should start with the coroutine thread rather than with Unitys main thread</param>
     /// <remarks>
     /// Tasks run on the ThreadPool, they should not be used for long-running operations, since they can fill up the thread pool and block new work.
     /// Instead, Task provides a LongRunning option, which will tell the TaskScheduler to spin up a new thread rather than running on the ThreadPool.
     /// </remarks>
-    public void StartCoroutineThread(ThreadedCoroutine coroutineThread, bool isLongRunning = false)
+    public void StartCoroutineThread(ThreadedCoroutine coroutineThread, bool isLongRunning = false, bool threadStarts = false)
     {
-        StartCoroutine(coroutineThread.Start(isLongRunning));
+        threadStarts = true;
+        // Making a bit harder for your programmers to call ThreadedCoroutine.Start() directly, you can remove this if you are familiar with this pattern
+        StartCoroutine(((IStartThreadedCoroutine)coroutineThread).StartWithCoroutineThread(isLongRunning, _cancellationTokenSource.Token));
     }
+
+    void OnDestroy()
+    {
+        _cancellationTokenSource.Cancel();
+        _cancellationTokenSource.Dispose();
+    }
+
 }
