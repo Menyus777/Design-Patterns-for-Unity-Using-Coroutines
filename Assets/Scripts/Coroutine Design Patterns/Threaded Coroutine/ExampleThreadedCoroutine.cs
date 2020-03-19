badddf6bc65e285f617ae7388b48cecb08c730bf
Menyus-Asus_G <menyus777@gmai.com>
2020-03-19 23:16:44 +0100

    Switched to a more robust solution managing ThreadedCoroutines

--- Assets/Scripts/Coroutine Design Patterns/Threaded Coroutine/ExampleThreadedCoroutine.cs
+++ Assets/Scripts/Coroutine Design Patterns/Threaded Coroutine/ExampleThreadedCoroutine.cs
@@ -3,45 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 
 public class ExampleThreadedCoroutine : ThreadedCoroutine
 {
-    Action Operations = null;
-    Func<string, GameObject> OperationWithReturnValue = null;
-    Action<Transform> OperationsTParam = null;
-
     GameObject Fred;
 
-    protected override void WorkOnUnityThread()
+    protected override IEnumerator WorkOnUnityThread()
     {
+        // Doing work on Unitys Main Thread
         Debug.Log("<color=yellow>WorkOnUnityThread method Thread ID:</color> " + Thread.CurrentThread.ManagedThreadId);
-        Operations?.Invoke();
-        Fred = OperationWithReturnValue?.Invoke("Fred");
-        OperationsTParam?.Invoke(Fred.transform);
+
+        // We then request a gameObject called "Fred" (its a cube) and multiply its position value on the Unity Thread
+        Fred = GameObject.Find("Fredv2");
+        Fred.transform.position *= 2;
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+        // Yield work to the coroutine thread
+        yield return RequestCoroutineThread();
+
+        // Now we change the gameobject color to blue indicating that we are finished with the process
+        Fred.GetComponent<Renderer>().material.color = Color.green;
+
+        yield return RequestCoroutineThread();
     }
 
-    protected override void WorkOnCoroutineThread()
+    protected override void WorkOnCoroutineThread(CancellationToken cancellationToken)
     {
-        Debug.Log("<color=yellow>WorkOnCoroutineThread method Thread ID:</color> " + Thread.CurrentThread.ManagedThreadId);
+        Debug.Log("<color=#00FF00>WorkOnCoroutineThread method Thread ID:</color> " + Thread.CurrentThread.ManagedThreadId);
         // Emulating some hardwork on the Coroutines thread at start.
         Thread.Sleep(3500);
-        // We then request a gameObject called "Fred" (its a cube) and multiply its position value on the Unity Thread
-        OperationWithReturnValue += (string gameObjectName) => GameObject.Find(gameObjectName);
-        OperationsTParam += (Transform transform) => transform.position *= 2;
-        RequestMainThread(); // Use request main thread to pause execution of the current thread and yield control to Unitys Thread
+
+        Debug.Log("<color=#00FF00>Requesting main thread...</color> " + Thread.CurrentThread.ManagedThreadId);
+        RequestMainThread(cancellationToken); // Use request main thread to pause execution of the current thread and yield control to Unitys Thread
 
         // After that another heavy workload takes place
         Thread.Sleep(2000);
 
-        // Now we change the gameobject color to green indicating that we are finished with the process
-        // Removing the previous method calls
-        OperationWithReturnValue = null;
-        OperationsTParam = null;
-        // Adding the new change color method
-        Operations += () => Fred.GetComponent<Renderer>().material.color = Color.green;
-        RequestMainThread(); // Use request main thread to pause execution of the current thread and yield control to Unitys Thread
-        Debug.Log("<color=#00FF00>Finished execution of thread</color>" + Thread.CurrentThread.ManagedThreadId);
-    }
+        RequestMainThread(cancellationToken); // Use request main thread to pause execution of the current thread and yield control to Unitys Thread
 
+        Debug.Log("<color=#00FF00>Finished execution of thread</color>" + Thread.CurrentThread.ManagedThreadId);
+        Finish();
+    }
 }
